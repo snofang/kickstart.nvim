d7f44d783b2c7c9e170d15afb89deadf239bb3f6
Mehdi Taghi <snofang@gmail.com>
2024-06-26 18:38:25 +0330

    adding toggleterm

---
+++ lua/kickstart/plugins/toggle_term.lua
@@ -0,0 +1,16 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  cmd = { 'ToggleTerm', 'TermExec' },
+  setup = true,
+  opts = {
+    size = 10,
+    open_mapping = [[<leader>tt]],
+    shading_factor = 2,
+    direction = 'float',
+    float_opts = {
+      border = 'curved',
+      highlights = { border = 'Normal', background = 'Normal' },
+    },
+  },
+}
