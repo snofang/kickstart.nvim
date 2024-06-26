51a1b3d60241759d59250809482d5d10ec53916d
Mehdi Taghi <snofang@gmail.com>
2024-06-26 18:42:39 +0330

    enabling toggleterm at the start

--- init.lua
+++ init.lua
@@ -962,5 +962,8 @@ require('lazy').setup({
   },
 })
 
+-- the plugin must be explicitly enabled
+require('toggleterm').setup {}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
