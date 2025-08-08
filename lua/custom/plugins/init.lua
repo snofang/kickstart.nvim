951879456993368fc058531eef81baa4d331530f
Mehdi Taghi <snofang@gmail.com>
2025-08-08 16:11:29 +0330

    feat: add markdown preview plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -48,4 +48,14 @@ return {
       vim.keymap.set('n', '<leader>c', close_buffer_safely, { desc = 'Close Buffer Safely' })
     end,
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    build = 'cd app && npm install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_auto_close = 0
+    end,
+    ft = { 'markdown' },
+  },
 }
